eed7b23ed53a6a66bd1fd1dfced21582355c62aa
Erika Nuñez <erikannunez@gmail.com>
2018-10-31 15:51:13 -0300

    proyecto en talend

---
+++ Talend/SSDTPO/poms/jobs/process/migration_0.1/src/main/templates/Job_run_template.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .;../lib/routines.jar;../lib/bcprov-jdk15on-1.51.jar;../lib/commons-lang-2.6.jar;../lib/commons-logging-1.1.3.jar;../lib/dom4j-1.6.1.jar;../lib/hsqldb.jar;../lib/jackcess-2.1.0.jar;../lib/jackcess-encrypt-2.1.0.jar;../lib/log4j-1.2.16.jar;../lib/mariadb-java-client-2.2.1.jar;../lib/talend-ucanaccess-utils-1.0.0.jar;../lib/ucanaccess-2.0.9.5.jar;migration_0_1.jar; ssdtpo.migration_0_1.Migration  %* 
